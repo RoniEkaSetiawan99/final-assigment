3b6ee95d95a92bcfde0812e977a61d7e762ba984
Roni Eka S <roni_eka.konsultan@work.bri.co.id>
2024-02-18 18:43:36 +0700

    done

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,4 +1,3 @@
-const timeout = 700;
 const home = `<section>
 <h2>Who <span class="highlightText">am i ?</span></h2>
 </section>
@@ -62,82 +61,6 @@ const work = `<section>
 </div>
 </article>`;
 
-function changeContent(location) {
-    const articleDiv = document.getElementById("article");
-
-    const content = location === 'work' ? work : home;
-
-    articleDiv.innerHTML = content;
+export {
+    home, work
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    var offcanvasDismissButton = document.querySelector('[data-bs-dismiss="offcanvas"]');
-    
-    setTimeout(function () {
-        document.body.classList.add('loaded');
-        document.getElementById("loader").remove();
-    }, timeout);
-
-    setTimeout(() => {
-        offcanvasDismissButton.click();
-    }, timeout + 150);
-
-    class TxtType {
-        constructor(el, toRotate, period) {
-            this.toRotate = toRotate;
-            this.el = el;
-            this.loopNum = 0;
-            this.period = parseInt(period, 10) || 1000;
-            this.txt = '';
-            this.tick();
-            this.isDeleting = false;
-        }
-        tick() {
-            var i = this.loopNum % this.toRotate.length;
-            var fullTxt = this.toRotate[i];
-
-            if (this.isDeleting) {
-                this.txt = fullTxt.substring(0, this.txt.length - 1);
-            } else {
-                this.txt = fullTxt.substring(0, this.txt.length + 1);
-            }
-
-            this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-            var that = this;
-            var delta = 200 - Math.random() * 100;
-
-            if (this.isDeleting) { delta /= 2; }
-
-            if (!this.isDeleting && this.txt === fullTxt) {
-                delta = this.period;
-                this.isDeleting = true;
-            } else if (this.isDeleting && this.txt === '') {
-                this.isDeleting = false;
-                this.loopNum++;
-                delta = 500;
-            }
-
-            setTimeout(function () {
-                that.tick();
-            }, delta);
-        }
-    }
-
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
-});
